8119920f1a405e6af3512b074c37ee1a3cca9742
serg113 <sergohnn@gmail.com>
2019-07-14 23:14:56 +0400

    rewritten with class Filter

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -1,61 +1,118 @@
 #include <iostream>
 #include <unordered_set>
+#include <initializer_list>
+#include <list>
+#include <iterator>
+#include <vector>
 
 template <typename T>
-std::unordered_set<T> array_to_set(const T * arr, const size_t sz)
+class Filter
 {
-	std::unordered_set<T> set;
 
-	for(int i = 0; i != sz; i++)
-	{
-		set.insert(arr[i]);
-	}
+	typedef typename std::vector<T>::const_iterator it_v;
+	typedef typename std::list<T>::const_iterator it_l;
 
-	return set;		
-}
+	std::unordered_set<T> (Filter<T>::*filter_dupl)() const;	
+	
+	std::vector<T> vec_org;
+	std::list<T> list_org;
+
+	public:
+		Filter() = delete;
+		
+		Filter(std::initializer_list<T> list)
+		{
+			std::cout << "constactor call --> initializer list" << std::endl;
+
+			for(auto& item : list) // may be initialized without loop ???
+			{
+				list_org.push_back(item);
+			}
+		
+			filter_dupl = &Filter<T>::filter_duplicates_list;
+		}
+		
+		Filter(it_v begin, it_v end)
+		{
+			std::cout << "constructor call --> vector iterator case" << std::endl;
+
+			for(auto it = begin; it != end; it++)
+			{
+				vec_org.push_back(*it);
+			}
+			
+			this->filter_dupl = &Filter<T>::filter_duplicates_vector;
+		}
+
+		Filter(it_l begin, it_l end)
+		{
+			std::cout << "constructor call --> list iterator case" << std::endl;
+
+			for(auto it = begin; it != end; it++)
+			{
+				list_org.push_back(*it);
+			}
+	
+			this->filter_dupl = &Filter::filter_duplicates_list;
+		}
+
+		std::unordered_set<T> get_without_duplicates() const
+		{
+			return (this->*filter_dupl)();
+		}
+
+	private:
+
+		std::unordered_set<T> filter_duplicates_list() const
+		{
+			std::unordered_set<T> set;
+
+			for(auto& item : list_org)
+			{
+				set.insert(item);
+			}
+
+			return set;
+		}
+
+		std::unordered_set<T> filter_duplicates_vector() const
+		{
+			std::unordered_set<T> set;
+
+			for(auto& item : vec_org)
+			{
+				set.insert(item);
+			}
+
+			return set;
+		}
 
 
-template <typename T> 
-void convert_set_to_array(const std::unordered_set<T> set, T *arr)
-{
-	size_t sz = 0;
-
-	for(T elm : set)
-	{
-		arr[sz] = elm;
-		sz++;
-	}
-}
-
+};
 
 
 int main()
 {
-	int sz = 5;
-
-	int input_array[sz] = {1, 2, 3, 2, 1};
-
-	std::unordered_set<int> set = array_to_set(input_array, sz);
+	Filter<char> filter{'a', 'b', 'c', 'a', 'c'};
 	
-	int output_array[set.size()];
-	
-	convert_set_to_array(set, output_array);
-	
+	auto char_set = filter.get_without_duplicates();
 
-
-	std::cout << "input array with duplicates: ";
-	for(auto elm : input_array)
+	for(auto i : char_set) 
 	{
-		std::cout << elm << " ";
+		std::cout << i << " ";
 	}
 	std::cout << std::endl;
 
-	std::cout << "output array without duplicates: ";
-	for(auto elm : output_array)
+
+	std::list<int> vec{1, 2, 3, 4, 3, 2, 1};
+	
+	Filter<int> filt(vec.cbegin(), vec.cend());
+
+	auto int_set = filt.get_without_duplicates();
+	
+	for(auto i : int_set)
 	{
-		std::cout << elm << " ";
+		std::cout << i << " ";
 	}
 	std::cout << std::endl;
-
-	return 0;
 }
