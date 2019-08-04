e35d5e02033db82784bf5788ef6c8c0fb4b3be02
serg113 <sergohnn@gmail.com>
2019-08-04 20:43:47 +0400

    commands writing to file in reverse order

--- command_history_manager.cpp
+++ command_history_manager.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <list>
 
 namespace cmh
 {
 	/**
-	 * todo: 
+	 * todo:
 	 * 	1. limit commands count to 10
-	 * 	2. get commands in reversed order
+	 * 	
 	 */
 
 	class CommandHistoryManager
 	{
 		public:
 			bool end_of_log {false};
-
+		
 		private:
+			std::string log_file;
 		
 			std::fstream comlog;
-
+			std::list<std::string> prev_commands;
+			std::list<std::string>::iterator prev_command = prev_commands.begin();
+			
+			//bool comlog_is_open {false};
 
 		public:
 			CommandHistoryManager(){};
@@ -30,7 +35,7 @@ namespace cmh
 			
 			~CommandHistoryManager()
 			{
-				comlog.close();
+				close();
 				
 				std::cout << "~CommnadHistoryManager(): comlog->closed" << std::endl;
 			}
@@ -40,39 +45,49 @@ namespace cmh
 				init(file_path);
 			}
 
+			void close()
+			{
+				if(comlog.is_open())
+				{
+					for(auto command : prev_commands)
+					{
+						comlog << command << std::endl;
+
+						std::cout << "written row: " << command << std::endl;
+					}
+					comlog.close();
+				}
+			}
+
 			void save_command(const std::string & command)
 			{
-				if(!comlog.is_open()) throw "[write error]";
-
-				comlog << command << std::endl;
+				prev_commands.insert(prev_commands.begin(), command);
 				
-				comlog.seekg(0);
+				prev_command = prev_commands.begin();
 			}
 
 			std::string get_prev_command()
-			{
-				if(!comlog.is_open()) throw "[read error]";
-				
-				std::string command;
-				
-				std::getline(comlog, command);
-				
-				if(comlog.eof()==1) 
-				{
-					std::cout << "[end of file]" << std::endl;
-					end_of_log = true;
-				}
-
-				return command;
+			{			
+				return *(++prev_command);
 			}
+
 		private:
 			void init(const std::string & file_path)
 			{
-				comlog.open(file_path, std::fstream::in | std::fstream::out | std::fstream::app);
+				if(!comlog.is_open())
+					comlog.open(file_path, std::fstream::in | std::fstream::out);
 				
-				if(!comlog.is_open()) throw "[init error]";
-			}
+				std::string command;
 
+				while(std::getline(comlog, command))
+				{
+					prev_commands.push_back(command);
+				}		
+
+				comlog.clear();
+			
+				comlog.seekg(0);
+			}
 	};
 }
 
@@ -90,16 +105,23 @@ int main()
 	}
 
 	std::string command;
+
+	for(int i = 0; i < 2; ++i)
+	{
 	
-	std::cout << "write command here ~/ ";
+		std::cout << "write command here: ";
 
-	std::cin >> command;
+		std::cin >> command;
 
-	hist_manager.save_command(command);
+		hist_manager.save_command(command);
+	
+		std::cout << "written command: " << hist_manager.get_prev_command() << std::endl;
+	}
+	hist_manager.close();
 
-	while(!hist_manager.end_of_log)
+	/*while(!hist_manager.end_of_log)
 	{
 		std::cout << "written command ~/ " << hist_manager.get_prev_command() << std::endl;
-	}
+	}*/
 
 }
