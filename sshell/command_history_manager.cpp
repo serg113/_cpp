3b98df6de18624e41622be695eefdbce292b5012
serg113 <sergohnn@gmail.com>
2019-09-08 21:59:00 +0400

    modified to use with header file

--- sshell/command_history_manager.cpp
+++ sshell/command_history_manager.cpp
@@ -1,138 +1,83 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <list>
-#include <iterator>
+#include "command_history_manager.h"
 
-namespace cmh
+cmh::CommandHistoryManager::CommandHistoryManager(){};
+cmh::CommandHistoryManager::CommandHistoryManager(const std::string& file_path)
 {
-	/**
-	 * todo: priority:
-	 *    
-	 *    2. 	1. limit commands count to 10
-	 * 	
-	 */
-
-	class CommandHistoryManager
-	{
-		private:
-			std::string log_file;
-		
-			std::fstream comlog;
-		
-			std::list<std::string> prev_commands;
-			std::list<std::string>::iterator prev_command = prev_commands.begin();
-			
-
-		public:
-			CommandHistoryManager(){};
-
-			CommandHistoryManager(const std::string & file_path)
-			{
-				init(file_path);
-			}
-			
-			~CommandHistoryManager()
-			{		
-				std::cout << "[cmh]: ~CommnadHistoryManager()" << std::endl;
-			
-				save_history();
-			}
-
-			void open(const std::string & file_path)
-			{
-				if(file_path != log_file)
-					save_history();
-
-				init(file_path);
-			}
-
-			void save_history()
-			{
-				if(comlog.is_open())
-				{
-					std::cout << "[cmh]: save_history(), writing commands to file" << std::endl;
-
-					for(auto command : prev_commands)
-					{
-						comlog << command << std::endl;
-
-						std::cout << "written command: " << command << std::endl;
-					}
-					comlog.close();
-				}
-			}
-
-			void save_command(const std::string & command)
-			{
-				prev_commands.insert(prev_commands.begin(), command);
-				
-				prev_command = prev_commands.begin();
-			}
-
-			std::string get_prev_command()
-			{
-				if(prev_commands.empty())
-					return "";
-
-				std::string ret_value = *prev_command;
-
-				if(++prev_command == prev_commands.end())  
-					--prev_command;
-
-				return ret_value;
-			}
-
-		private:
-			void init(const std::string & file_path)
-			{
-				log_file = file_path;
-
-				if(!comlog.is_open())
-					comlog.open(file_path, std::fstream::in | std::fstream::out);
-
-				if(!comlog.is_open())
-					comlog.open(file_path, std::fstream::in | std::fstream::out | std::fstream::trunc);
-				
-				std::string command;
-
-				while(std::getline(comlog, command))
-				{
-					prev_commands.push_back(command);
-				}
-				prev_command = prev_commands.begin();		
-
-				comlog.clear();	
-				comlog.seekg(0);
-			}
-	};
+	init(file_path);
 }
 
-int main()
+cmh::CommandHistoryManager::~CommandHistoryManager()
 {
-	std::string file = "command.log";
+	std::cout << "[cmh]: ~CommandHistoryManager()" << std::endl;
+	save_history();
+}
 
-	cmh::CommandHistoryManager m_history(file);
+void cmh::CommandHistoryManager::open(const std::string& file_path)
+{
+	if(file_path != log_file)
+		save_history();
+	
+	init(file_path);
+}
 
-
-	std::cout << "last three commands: ";
-	for(int i = 0; i < 3; i++)
-		std::cout << m_history.get_prev_command() << " >>> ";
-	std::cout << std::endl << std::endl;
-
-
-	std::string command;
-
-	for(int i = 0; i < 2; ++i)
+void cmh::CommandHistoryManager::save_history()
+{
+	if(comlog.is_open())
 	{
-	
-		std::cout << "write command here: ";
+		std::cout << "[cmh]: save_history(), writing commands to file" 
+			<< std::endl;
+		for(auto command : prev_commands)
+		{
+			comlog << command << std::endl;
 
-		std::cin >> command;
+			std::cout << "written command: " 
+				<< command << std::endl;
+		}
 
-		m_history.save_command(command);
-	
-		std::cout << "previous command was: " << m_history.get_prev_command() << std::endl;
+		comlog.close();
 	}
 
 }
+
+void cmh::CommandHistoryManager::save_command(const std::string& command)
+{
+	prev_commands.insert(prev_commands.begin(), command);
+	prev_command = prev_commands.begin();
+}
+
+
+std::string cmh::CommandHistoryManager::get_prev_command()
+{
+	if(prev_commands.empty())
+		return "";
+
+	std::string ret_value = *prev_command;
+
+	if(++prev_command == prev_commands.end())
+		--prev_command;
+
+	return ret_value;
+}
+
+void cmh::CommandHistoryManager::init(const std::string& file_path)
+{
+	//using comlog = cmh::CommandHistoryManager::comlog;
+	cmh::CommandHistoryManager::log_file = file_path;
+	if(!comlog.is_open())
+		comlog.open(file_path, std::fstream::in | std::fstream::out);
+	if(!comlog.is_open())
+		comlog.open(file_path,
+			std::fstream::in | std::fstream::out | std::fstream::trunc);
+	std::string command;
+
+	while(std::getline(comlog, command))
+	{
+		prev_commands.push_back(command);
+	}
+	prev_command = prev_commands.begin();
+
+	comlog.clear();
+	comlog.seekg(0);
+}
+
+
