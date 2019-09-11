214d4b45f97d0a14f85cc0d1d9606bdf8c3eba6d
serg113 <44881713+serg113@users.noreply.github.com>
2019-09-11 14:05:20 +0400

    dynamic programming sample, memoization

---
+++ range_of_fibonacci_memoization.cpp
@@ -0,0 +1,39 @@
+/*
+* Fibonacci numbers are calculated as F(n) = F(n-1) + F(n-2)
+* with condition, that F(0) = 0 and F(1) = 1
+* but interesting case is with memoization - dynamic programming
+* with memoization we can compute range of Fibonacci numbers very fast
+*/
+
+#include <iostream>
+#include <unordered_map>
+
+std::unordered_map<int, int> memo = {{0,0},{1,1}};
+
+int fib(int);
+
+int main()
+{
+    for(int i = 0; i <= 10; ++i)
+    {
+        std::cout << "fib(" << i << ") = " << fib(i) << std::endl;
+    }
+}
+
+int fib(int n)
+{
+    auto search_res = memo.find(n);
+
+    if(search_res != memo.end())
+    {
+        return search_res -> second;
+    }
+    else
+    {
+        int res = fib(n - 1) + fib(n - 2);
+
+        memo.emplace(std::make_pair(n, res));
+
+        return res;
+    }
+}
