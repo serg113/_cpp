dee76e7948b594092ac219117ac3b49b532c24ef
serg113 <44881713+serg113@users.noreply.github.com>
2019-12-31 18:28:47 +0400

    Create Session.cpp

---
+++ ssh_app/Session.cpp
@@ -0,0 +1,141 @@
+#include "Session.h"
+
+
+Session::Session() {};
+
+Session::~Session()
+{
+	if (sftp != NULL)
+	{
+		sftp_free(sftp);
+	}
+
+	if (session != NULL)
+	{
+		ssh_disconnect(session);
+		ssh_free(session);
+	}
+}
+
+
+Session& Session::Connect(const std::string &host, int port)
+{
+	session = ssh_new();
+
+	if (session == NULL)
+		throw std::exception("ssh session has not been initialized");
+
+	ssh_options_set(session, SSH_OPTIONS_HOST, host.c_str());
+	ssh_options_set(session, SSH_OPTIONS_PORT, &port);
+	//ssh_options_set(session, SSH_OPTIONS_LOG_VERBOSITY, &verb);
+	//ssh_options_set(ssh_session_, SSH_OPTIONS_USER, "user");
+
+	if (ssh_connect(session) == SSH_OK)
+	{
+		std::cout << "[ok] successfully connected" << std::endl;
+	}
+	else
+	{
+		throw std::exception("cannot connect to remote host");
+	}
+
+	return *this;
+}
+
+Session& Session::Login(const std::string &login, const std::string &password)
+{
+	if (ssh_userauth_password(session, NULL, password.c_str()) == SSH_AUTH_SUCCESS)
+	{
+		std::cout << "[ok] successfully authorized" << std::endl;
+	}
+	else
+	{
+		throw std::exception("cannot authorize session");
+	}
+
+	return *this;
+}
+
+Session& Session::CreateDir(const std::string &dir, int permissions)
+{
+	InitSftp();
+
+	std::size_t first_pos = 0, second_pos;
+
+	while (true)
+	{
+		second_pos = dir.find("/", first_pos);
+
+		if (second_pos != std::string::npos || dir.substr(first_pos).size() > 0)
+		{
+			int rc = sftp_mkdir(sftp, dir.substr(0, second_pos).c_str(), permissions);
+
+			if (rc != SSH_OK)
+			{
+				if (sftp_get_error(sftp) != SSH_FX_FILE_ALREADY_EXISTS)
+					std::cout << "[info] directory already exists: "
+					<< dir.substr(0, second_pos) << std::endl;
+			}
+			else
+			{
+				std::cout << "[ok] folder created : " << dir.substr(0, second_pos) << std::endl;
+			}
+			first_pos = second_pos + 1;
+
+			if (second_pos == std::string::npos)
+				break;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return *this;
+}
+
+Session& Session::SendFile(const std::string &source, const std::string &destination, int access_type, int permissions)
+{
+	InitSftp();
+
+	sftp_file remote_file = sftp_open(sftp, destination.c_str(), access_type, permissions);
+
+	if (remote_file == NULL)
+		throw std::exception("cannot open remote file");
+
+	std::ifstream local_file;
+
+	local_file.open(source.c_str(), std::ios::binary);
+
+	if (local_file.is_open())
+	{
+		std::string chunk;
+
+		while (std::getline(local_file, chunk))
+		{
+			std::cout << chunk << std::endl;
+
+			std::size_t written_bytes = sftp_write(remote_file, chunk.c_str(), chunk.size());
+
+			if (written_bytes != chunk.size())
+				throw std::exception("cannot write to remote file");
+		}
+	}
+	sftp_close(remote_file);
+
+	return *this;
+}
+
+void Session::InitSftp()
+{
+	if (sftp != NULL)
+		return;
+
+	sftp = sftp_new(session);
+
+	if (sftp == NULL)
+		throw std::exception("cannot allocate sftp session");
+
+	if (sftp_init(sftp) != SSH_OK)
+		throw std::exception("cannot initialize sftp session");
+}
