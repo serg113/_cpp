a7165bc2a0121a8332a4d6eeb72f1637b1f4a892
serg113 <44881713+serg113@users.noreply.github.com>
2020-01-01 18:53:14 +0400

    Update main.cpp

--- ssh_app/main.cpp
+++ ssh_app/main.cpp
@@ -2,23 +2,10 @@
 #include <fcntl.h>
 #include <vector>
 
+#include "ArgParser.h"
 #include "Session.h"
 
-class ArgParser
-{
-public:
-	ArgParser(const std::vector<std::string> &args);
 
-	std::string get_source_path() const;
-	std::string get_destination_path() const;
-	bool is_command_parsed() const;
-
-private:
-	std::string local_file;
-	std::string remote_file;
-	bool parsing_succeed = false;
-
-};
 
 int main(int argc, char* argv[])
 {
@@ -36,7 +23,7 @@ int main(int argc, char* argv[])
 
 	ArgParser args(arg_values);
 
-	if (!args.is_command_parsed() and !on_debug)
+	if (!args.is_command_parsed() && !on_debug)
 		return 0;
 	
 
@@ -47,21 +34,26 @@ int main(int argc, char* argv[])
 	
 	int file_access_type = O_WRONLY | O_CREAT | O_TRUNC; 
 	int file_permissions = 0700; //S_IRWXU = 0700 on linux, std::filesystem::perms::owner_all for C++17
-	
-	std::string source_file = "./test_file.dat";
-
 	int folder_permissions = file_permissions;
-	std::string dest_file = "f1/f2/test_dest.dat";
+	
+	std::string source_file;
+	std::string dest_file;
 
-	if (!on_debug)
+	if (on_debug)
 	{
-		std::string source_file = args.get_source_path();
-		std::string dest_file = args.get_destination_path();
+		source_file = "./test_file.dat";
+		dest_file = "f1/f2/test_dest.dat";
+	}
+	else
+	{
+		source_file = args.get_source_path();
+		dest_file = args.get_destination_path();
 	}
 	std::string dest_dir = dest_file.substr(0, dest_file.find_last_of("/\\"));
 
-	Session session;
+	std::cout << source_file << " : " << dest_file << std::endl;
 
+	Session session;
 	try
 	{
 		session.Connect(host, port)
@@ -79,54 +71,3 @@ int main(int argc, char* argv[])
 }
 
 
-ArgParser::ArgParser(const std::vector<std::string> & args)
-{
-	if (args.size() == 1)
-	{
-		if (args[0] == "-h" || args[0] == "--help")
-		{
-			std::cout << "currently implemented commands: \n" << std::endl;
-			std::cout << "ssh_app.exe -rmcopy [source path] [destination path] " << std::endl;
-		}
-		else
-		{
-			std::cout << "type -h or --help to see the list of available commands" << std::endl;
-		}
-	}
-	else if (args.size() == 3)
-	{
-		if (args[0] == "-rmcopy")
-		{
-			local_file = args[1];
-			remote_file = args[2];
-
-			std::replace(local_file.begin(), local_file.end(), '\\', '/');
-			std::replace(remote_file.begin(), remote_file.end(), '\\', '/');
-
-			parsing_succeed = true;
-		}
-		else
-		{
-			std::cout << "type -h or --help to see the list of available commands" << std::endl;
-		}
-	}
-	else
-	{
-		std::cout << "type -h or --help to see the list of available commands" << std::endl;
-	}
-
-
-}
-
-std::string ArgParser::get_source_path() const
-{
-	return local_file;
-}
-std::string ArgParser::get_destination_path() const
-{
-	return remote_file;
-}
-bool ArgParser::is_command_parsed() const
-{
-	return parsing_succeed;
-}
