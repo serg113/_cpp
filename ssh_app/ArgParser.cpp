81f7700cb248b3f2586f86309d135d724722dcda
serg113 <44881713+serg113@users.noreply.github.com>
2020-01-01 18:56:04 +0400

    Create ArgParser.cpp

---
+++ ssh_app/ArgParser.cpp
@@ -0,0 +1,54 @@
+#include "ArgParser.h"
+
+
+ArgParser::ArgParser(const std::vector<std::string> & args)
+{
+	if (args.size() == 1)
+	{
+		if (args[0] == "-h" || args[0] == "--help")
+		{
+			std::cout << "currently implemented commands: \n" << std::endl;
+			std::cout << "ssh_app.exe -rmcopy [source path] [destination path] " << std::endl;
+		}
+		else
+		{
+			std::cout << "type -h or --help to see the list of available commands" << std::endl;
+		}
+	}
+	else if (args.size() == 3)
+	{
+		if (args[0] == "-rmcopy")
+		{
+			local_file = args[1];
+			remote_file = args[2];
+
+			std::replace(local_file.begin(), local_file.end(), '\\', '/');
+			std::replace(remote_file.begin(), remote_file.end(), '\\', '/');
+
+			parsing_succeed = true;
+		}
+		else
+		{
+			std::cout << "type -h or --help to see the list of available commands" << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "type -h or --help to see the list of available commands" << std::endl;
+	}
+
+
+}
+
+std::string ArgParser::get_source_path() const
+{
+	return local_file;
+}
+std::string ArgParser::get_destination_path() const
+{
+	return remote_file;
+}
+bool ArgParser::is_command_parsed() const
+{
+	return parsing_succeed;
+}
