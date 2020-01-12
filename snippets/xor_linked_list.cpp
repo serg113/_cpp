b07b76f69e6c543aa240a86bed2e6f8a609662fb
serg113 <44881713+serg113@users.noreply.github.com>
2020-01-12 18:31:00 +0400

    xor linked list

--- snippets/xor_linked_list.cpp
+++ snippets/xor_linked_list.cpp
@@ -5,82 +5,83 @@
 
 class Node
 {
-    size_t xor_ = 0;
-
 public:
     friend class NodeList;
 
-    Node(std::string name)
-    {
-        name_ = name;
-    }
+    Node(std::string&& name): name_(name) {};
+
     std::string name_;
+
+private:
+    size_t xor_value_ = 0;
 };
 
-struct NodeList
+class NodeList
 {
-    Node* root = nullptr;
-
-    NodeList& Create()
+public:
+    NodeList& Create(std::string&& name)
     {
-        root = new Node("root");
+        root_ = new Node(std::move(name));
         return *this;
     }
 
     NodeList& add(Node* node)
-    {
-        if(root != nullptr)
-        {
-            Node* fnode = root;
+    {//if(root_ == nullptr) return nullptr;
 
-            while(true)
+        Node* it_node = root_;
+
+        while(true)
+        {
+            if(it_node->xor_value_ != 0)
             {
-                if(fnode->xor_ != 0)
-                {
-                    fnode = (Node*)(fnode->xor_^((size_t)fnode));
-                }
-                else
-                {
-                    fnode->xor_ = (size_t)fnode^(size_t)node;
-                    break;
-                }
+                it_node = (Node*)(it_node->xor_value_ ^ ((size_t)it_node));
+            }
+            else
+            {
+                it_node->xor_value_ = (size_t)it_node ^ (size_t)node;
+                break;
             }
         }
         return *this;
     }
 
-    Node* get(int index)
-    {
-        if(root == nullptr) return nullptr;
+    Node& get(int index)
+    {//if(root_ == nullptr) throw exception;
+        if(index == 0) return *root_;
 
-        if(index == 0) return root;
-
-        Node* fnode = root;
+        Node* it_node = root_;
 
         for(int i = 1; i <= index; ++i)
-        {
-            if(fnode->xor_ == 0) return nullptr;
-
-            fnode = (Node*)(fnode->xor_^((size_t)fnode));
+        {//if(it_node->xor_ == 0) throw out of range exception;
+            it_node = (Node*)(it_node->xor_value_ ^ ((size_t)it_node));
         }
 
-        return fnode;
+        return *it_node;
     }
 
+private:
+    Node* root_ = nullptr;
+
+    Node* count_xor(size_t val, Node* node)
+    {
+        size_t node_rp = reinterpret_cast<size_t>(node);
+
+        return reinterpret_cast<Node*>((node->xor_value_ ^ node_rp));
+    }
 };
 
 
 int main()
 {
-    NodeList nodes = NodeList().Create();
+    NodeList nodes = NodeList().Create("root");
 
     nodes.add(new Node("n1"))
                 .add(new Node("n2"))
                     .add(new Node("n3"));
 
-    std::cout <<"get(0).name_ = " << nodes.get(0)->name_ << std::endl;
-    std::cout <<"get(1).name_ = " << nodes.get(1)->name_ << std::endl;
-    std::cout <<"get(2).name_ = " << nodes.get(2)->name_ << std::endl;
+    std::cout <<"get(0).name_ = " << nodes.get(0).name_ << std::endl;
+    std::cout <<"get(1).name_ = " << nodes.get(1).name_ << std::endl;
+    std::cout <<"get(2).name_ = " << nodes.get(2).name_ << std::endl;
 
     return 0;
 }
