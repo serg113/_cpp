0b175edf77a3dc6cb9afaa6009e08461480b2c8f
serg113 <44881713+serg113@users.noreply.github.com>
2020-02-16 13:21:31 +0400

    NotConnected interface added

--- ssh_app_v2/SessionBase.h
+++ ssh_app_v2/SessionBase.h
@@ -1,28 +1,41 @@
 #include <string>
+#include <memory>
 
 #ifndef SA_SESSIONBASE_H
 #define SA_SESSIONBASE_H
 
-class NotInitializedSession;
+class AuthorizedSession;
+class NotAuthorizedSession;
+class NotConnectedSession;
 
-class InitializedSession
+using InitedSess_ = std::shared_ptr<AuthorizedSession>;
+using UnInitedSess_ = std::shared_ptr<NotAuthorizedSession>;
+using UnConnectedSess_ = std::shared_ptr<NotConnectedSession>;
+
+class AuthorizedSession
 {
 public:
-	virtual InitializedSession& CreateDir(const std::string &dir, int permissions) = 0;
+	virtual InitedSess_ CreateDir(const std::string &dir, int permissions) = 0;
 
-	virtual InitializedSession& SendFile(const std::string &source,
-		const std::string &destination,
-		int access_type,
-		int permissions,
-		bool create_dir = false
-	) = 0;
-	virtual NotInitializedSession& LogOut() = 0;
+	virtual InitedSess_ SendFile(const std::string &source,
+									const std::string &destination,
+										int access_type,
+											int permissions,
+												bool create_dir = false
+													) = 0;
+	virtual UnInitedSess_ LogOut() = 0;
 };
 
-class NotInitializedSession
+class NotAuthorizedSession
 {
 public:
-	virtual InitializedSession& Login(const std::string &login, const std::string &password) = 0;
+	virtual InitedSess_ Login(const std::string &login, const std::string &password) = 0;
+};
+
+class NotConnectedSession
+{
+public:
+	virtual UnInitedSess_ Connect(const std::string &host, int port) = 0;
 };
 
 #endif // SA_SESSIONBASE_H
